47c62977458f0e809bb77f705c643216e563eb89
Damien MM <damien.martinsmarques@ynov.com>
2022-02-10 10:09:11 +0100

    Refactoring et improvements

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css'
 import {Card} from "./Card";
-import getCurrentAndNextSeason from "./getCurrentAndNextSeason";
 import Modal from "./Modal";
 import {Button} from "./Button";
 import {useState} from "react";
+import getCurrentSeason from "./getCurrentSeason";
 
 export function App() {
-    let {dateSaisonSuivante, depuisNext, duree, dateSaisonActuel, depuisCurrent} = getCurrentAndNextSeason()
+    let  {dateSaisonActuel, depuisCurrent, dateSaisonNext, depuisNext, dureeNext} = getCurrentSeason()
     const [open, setOpen] = useState(false)
     const onClick = () => setOpen(!open)
     return <div className={'App'}>
-        <Card name={dateSaisonActuel.name} depuis={depuisCurrent} />
+        <Card name={dateSaisonActuel.season} depuis={depuisCurrent} />
         <Modal open={open} setOpen={setOpen}>
-            <Card name={dateSaisonSuivante.name} depuis={depuisNext} duree={duree} />
+            <Card name={dateSaisonNext.season} depuis={depuisNext} duree={dureeNext} />
         </Modal>
         <div className='Button'>
             <Button onClick={onClick}>Afficher la saison suivante</Button>
--- src/getCurrentSeason.js
+++ src/getCurrentSeason.js
@@ -1,23 +1,50 @@
 import {dates} from "./dates";
-import {differenceInMonths, formatDistanceToNow} from "date-fns";
+import {differenceInDays, formatDistanceToNow, isWithinInterval} from "date-fns";
 import fr from "date-fns/locale/fr";
 
 export default function getCurrentSeason(){
-    let lastResultat = null
-    let dateSaisonActuel = null
     let now = new Date()
-    dates.map(d => {
-        let result = differenceInMonths(now, d.value)
-        if(!result || result > lastResultat){
-            lastResultat = result
-            dateSaisonActuel = d
-        }
-    })
-    const depuisCurrent = formatDistanceToNow(dateSaisonActuel.value,
+    let dateSaisonActuel = getSeason(now)
+    let nextSeason = getNextSeason(dateSaisonActuel.season)
+    let dateSaisonNext = dates.filter(d => d.season === nextSeason)[0]
+
+    const depuisCurrent = formatDistanceToNow(dateSaisonActuel.debut,
         {
             locale: fr,
             addSuffix: true
         }
     )
-    return {dateSaisonActuel, depuisCurrent}
+    const depuisNext = formatDistanceToNow(dateSaisonNext.debut,
+        {
+            locale: fr,
+            addSuffix: true
+        }
+    )
+    const dureeNext =  differenceInDays(
+        dateSaisonNext.debut,
+        dateSaisonNext.fin
+    )
+    return {dateSaisonActuel, depuisCurrent, dateSaisonNext, depuisNext, dureeNext}
+}
+function getSeason(date) {
+    return dates.filter(d => {
+        if(isWithinInterval(date, {
+            start: d.debut,
+            end: d.fin
+        })){
+            return d
+        }
+    })[0]
+}
+function getNextSeason(season) {
+    switch (season){
+        case "Hiver":
+            return "Printemps"
+        case "Printemps":
+            return "Ete"
+        case "Ete":
+            return "Automne"
+        case "Automne":
+            return "Hiver"
+    }
 }
